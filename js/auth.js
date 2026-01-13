92961ff9c66bf9620e11080da476848e35f5563d
mouali555 <100873378+mouali555@users.noreply.github.com>
2026-01-13 23:30:32 +0000

    Switch

---
+++ js/auth.js
@@ -0,0 +1,36 @@
+// js/auth.js
+import { auth, db } from "./firebase.js";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+
+import {
+  doc, setDoc, serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+
+const provider = new GoogleAuthProvider();
+
+export async function loginGoogle() {
+  const res = await signInWithPopup(auth, provider);
+
+  // Profil minimal (utile pour la suite)
+  await setDoc(doc(db, "users", res.user.uid), {
+    uid: res.user.uid,
+    displayName: res.user.displayName || "user",
+    photoURL: res.user.photoURL || null,
+    updatedAt: serverTimestamp()
+  }, { merge: true });
+
+  return res.user;
+}
+
+export function logout() {
+  return signOut(auth);
+}
+
+export function watchAuth(cb) {
+  return onAuthStateChanged(auth, cb);
+}
