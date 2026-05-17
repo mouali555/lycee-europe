84708d6d785ea54435c92a9ca3845458971d67b5
Yuki <yukitoo@outlook.fr>
2026-05-17 04:28:41 +0200

    deploy: add .nojekyll and copy assets to root

---
+++ _nuxt/BlyrEQVj.js
@@ -0,0 +1,1 @@
+import{L as a,J as s,l as r,o as u,n as o}from"./zQV5ehwl.js";function i(e){const t=e||s();return t?.ssrContext?.head||t?.runWithContext(()=>{if(r())return u(o)})}function x(e,t={}){const n=i(t.nuxt);if(n)return a(e,{head:n,...t})}export{x as u};
---
+++ _nuxt/BqVlixGp.js
@@ -0,0 +1,1 @@
+import"./zQV5ehwl.js";const r=""+new URL("video-fond.CHAxDf7V.mp4",import.meta.url).href;export{r as _};
